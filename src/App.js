cf9c4114a55edc6d4be24816c16d72162408c7c2
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-27 23:12:41 -0500

    added components such as Footer.js and Header.js and MemberCard.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MemberList from './MemberList';
 import MemberDetail from './MemberDetail';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router basename="/kguc-album-first">
+      <Header />
       <Routes>
         <Route path="/" element={<MemberList />} />
         <Route path="/member/:id" element={<MemberDetail />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Church Name. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
