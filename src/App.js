4a809ac90f689421e520fde0ff1757510a06e204
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-01 15:47:08 -0500

    modified files and added new components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MemberList from './MemberList';
 import MemberDetail from './MemberDetail';
+import FilteredMembersList from './FilteredMembersList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SkillsDropdown from './components/SkillsDropdown';
+
+
+// List of unique skills for the dropdown
+const skills = [
+  'Medical Doctor',
+  'Engineer',
+  'Choir Member',
+  'Technical Team',
+  'Young Adult Movement',
+  'Youth Fellowship',
+  'Sunday School Teacher',
+  'Pharmacist',
+  'Chemical Engineer',
+  'Youth Leader',
+  'Minister',
+  'Entrepreneur',
+  'Businesswoman',
+  'Businessman',
+  'Usher'
+];
+
 
 function App() {
   return (
-<Router basename={process.env.PUBLIC_URL}>
-
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
+      <SkillsDropdown skills={skills} />
       <Routes>
         <Route path="/" element={<MemberList />} />
         <Route path="/member/:id" element={<MemberDetail />} />
+        <Route path="/skills/:skill" element={<FilteredMembersList />} />
       </Routes>
       <Footer />
     </Router>
--- src/membersData.js
+++ src/membersData.js
@@ -3,97 +3,86 @@ const members = [
     id: 1,
     name: 'Natalya Rose',
     profilePicture: process.env.PUBLIC_URL + '/images/natalya.jpg',
-    description: 'Church: Founding member of the original Powerpoint team: Career: Medical Doctor.'
+    description: 'Founding member of the original Powerpoint team.',
+    skills: ['Medical Doctor', 'Technical Team']
   },
   {
     id: 2,
     name: 'Jane Smith',
-
     profilePicture: process.env.PUBLIC_URL + '/images/jane.jpg',
-    description: 'Sunday School teacher and coordinator of community outreach programs.'
+    description: 'Sunday School teacher and coordinator of community outreach programs.',
+    skills: ['Sunday School Teacher', 'Community Outreach', 'Usher']
   },
- {
+  {
     id: 3,
     name: 'Gladstone Rose',
     profilePicture: process.env.PUBLIC_URL + '/images/gladstone.jpg',
-    description: 'Secretary for the Planning Committee, senior member of Daniel House and member of the Technical Team.'
+    description: 'Secretary for the Planning Committee and member of the Technical Team',
+    skills: ['Planning Committee', 'Technical Team', 'Entrepreneur']
   },
-{
+  {
     id: 4,
     name: 'Alaina Rose',
     profilePicture: process.env.PUBLIC_URL + '/images/alaina.jpg',
-    description: 'Church:Youth Fellowship Co-ordinator and member of the Technical Team - Career: Engineer.'
+    description: 'Young Adult Co-ordinator, Technical Team member and Engineer.',
+    skills: ['Engineer', 'Young Adult Movement', 'Technical Team', 'Youth Leader']
   },
-{
+  {
     id: 5,
     name: 'Jeaneth Rose',
     profilePicture: process.env.PUBLIC_URL + '/images/jeaneth.jpg',
-    description: 'Church: Choir Member and Deputy Board Chairman - Career: Quality Manager.'
+    description: 'Choir Member and Deputy Board Chairman.',
+    skills: ['Choir Member', 'Quality Manager']
   },
-{
+  {
     id: 6,
-    name: 'Nadia Rose',
-    profilePicture: process.env.PUBLIC_URL + '/images/nadia.jpg',
-    description: 'Church: Senior Member - Career: Medical Doctor.'
-  },
-{
-    id: 7,
     name: 'John Brown',
     profilePicture: process.env.PUBLIC_URL + '/images/john.jpg',
-    description: 'Church: This is a sample - Career: Sample Profession.'
+    description: 'Businessman and member of the community outreach team.',
+    skills: ['Businessman', 'Community Outreach', 'Usher']
   },
-{
+  {
+    id: 7,
+    name: 'Nadia Rose',
+    profilePicture: process.env.PUBLIC_URL + '/images/nadia.jpg',
+    description: 'Senior member and member of the community outreach team.',
+    skills: ['Medical Doctor', 'Community Outreach', 'Usher']
+  },
+  {
     id: 8,
-    name: 'Sample One',
-    profilePicture: process.env.PUBLIC_URL + '/images/sample1.jpg',
-    description: 'Church: This is a sample - Career: Sample Profession.'
-  },
-{
-    id: 9,
-    name: 'Sample Two',
-    profilePicture: process.env.PUBLIC_URL + '/images/sample2.jpg',
-    description: 'Church: This is a sample - Career: Sample Profession.'
-  },
-{
-    id: 10,
-    name: 'Sample Three',
-    profilePicture: process.env.PUBLIC_URL + '/images/sample3.jpg',
-    description: 'Church: This is a sample - Career: Sample Profession.'
-  },
-{
-    id: 11,
     name: 'Mikhail Reid',
     profilePicture: process.env.PUBLIC_URL + '/images/mikhail.jpg',
-    description: 'Church: Praise Team Member - Career: Chemical Engineer.'
+    description: 'Youth Leader, member of the Technical Team, Keyboard Player and member of the community outreach team.',
+    skills: ['Chemical Engineer', 'Community Outreach', 'Technical Team', 'Young Adult Movement', 'Choir Member']
   },
-{
-    id: 12,
+  {
+    id: 9,
     name: 'Glodine Anderson',
     profilePicture: process.env.PUBLIC_URL + '/images/glodine.jpg',
-    description: 'Church: Praise Team Member - Career: Pharmacist.'
+    description: 'Choir member, Pharmacist and member of the community outreach team.',
+    skills: ['Pharmacist', 'Community Outreach', 'Choir Member']
   },
-{
-    id: 13,
-    name: 'Jahmai Paul',
-    profilePicture: process.env.PUBLIC_URL + '/images/jahmai.jpg',
-    description: 'Church: Member of Praise team - Career: Entrepreneur.'
+  {
+    id: 10,
+    name: 'Rev Garwell Bacchas',
+    profilePicture: process.env.PUBLIC_URL + '/images/garwell.jpg',
+    description: 'Chairman of the Board of Elders, Ordained Minister of Religion.',
+    skills: ['Minister', 'Community Outreach', 'Planning Committee']
   },
-{
-    id: 14,
+  {
+    id: 11,
     name: 'Carleen Soares',
     profilePicture: process.env.PUBLIC_URL + '/images/carleen.jpg',
-    description: 'Church: Usher, member of Daniel House - Career: Business Woman.'
+    description: 'Usher and member of the community outreach team.',
+    skills: ['Businesswoman', 'Community Outreach', 'Usher']
   },
-{
-    id: 15,
-    name: 'Rev. Garwell Bacchas',
-    profilePicture: process.env.PUBLIC_URL + '/images/garwell.jpg',
-    description: 'Church: Minister, Chairman of the Board - Career: Ordained Minister.'
+  {
+    id: 12,
+    name: 'Jahmai Paul',
+    profilePicture: process.env.PUBLIC_URL + '/images/jahmai.jpg',
+    description: 'Usher and member of the community outreach team.',
+    skills: ['Businesswoman', 'Community Outreach', 'Young Adult Movement', 'Choir Member']
   }
-
-
-
-
 
 
 
